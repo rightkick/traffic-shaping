93328bb30bc3b9e9a1d235d67d6d5cd9fa23300f
Alex Kaouris <akaouris@mtis.tech>
2023-11-17 16:00:18 +0200

    Improvements

--- traffic-shaper.sh
+++ traffic-shaper.sh
@@ -39,70 +39,83 @@ WANIP2=192.168.29.64
 echo "$timestamp: $base: traffic shaper started" >> $log
 
 # Do the job
-if [ "${1}" = "clear" ];then
-	for DEV in {$SAT1,$SAT2};do
-		echo "Cleaning traffic queues on interface $DEV"
-		tc qdisc del dev $DEV root > /dev/null 2>&1
-		tc qdisc del dev ifb0 root > /dev/null 2>&1
-	done
-	exit 0
-else
-	for DEV in $SAT1;do
+case "${1}" in
+	"clear")
+		for DEV in {$SAT1,$SAT2,ifb0};do
+			echo "Cleaning traffic queues on interface $DEV"
+			tc qdisc del dev $DEV root > /dev/null 2>&1
+		done
+		exit 0
+		;;
+	"stats")
+		for DEV in {$SAT1,$SAT2,ifb0};do
+			echo "QoS stats at interface $DEV"
+			tc -s qdisc show dev $DEV
+		done
+		;;
+	"help")
+		echo "Just run the script to enable QoS"
+		echo "Usage:"
+		echo $base <|stats|help>
+		;;
+	*)
+		for DEV in $SAT1;do
 
-		echo "Applying traffic shaping at interface: " $DEV
-		tc qdisc del dev $DEV root > /dev/null 2>&1 || echo error at line $LINENO
+			echo "Applying traffic shaping at interface: " $DEV
+			tc qdisc del dev $DEV root > /dev/null 2>&1 || echo error at line $LINENO
 
-		tc qdisc add dev $DEV parent root handle 1: hfsc default 10 || echo error at line $LINENO
-		tc class add dev $DEV parent 1: classid 1:1 hfsc ls rate 100mbit ul rate 100mbit || echo error at line $LINENO
-		tc class add dev $DEV parent 1:1 classid 1:10 hfsc ls rate 90mbit ul rate 90mbit || echo error at line $LINENO
+			tc qdisc add dev $DEV parent root handle 1: hfsc default 10 || echo error at line $LINENO
+			tc class add dev $DEV parent 1: classid 1:1 hfsc ls rate 100mbit ul rate 100mbit || echo error at line $LINENO
+			tc class add dev $DEV parent 1:1 classid 1:10 hfsc ls rate 90mbit ul rate 90mbit || echo error at line $LINENO
 
-		tc class add dev $DEV parent 1:1 classid 1:11 hfsc ls rate "${down1}" ul rate "${down1}" || echo error at line $LINENO
-		tc qdisc add dev $DEV parent 1:11 handle 20 netem delay "${latency1}" $deviation1 loss "${packet_loss1}" || echo error at line $LINENO
+			tc class add dev $DEV parent 1:1 classid 1:11 hfsc ls rate "${down1}" ul rate "${down1}" || echo error at line $LINENO
+			tc qdisc add dev $DEV parent 1:11 handle 20 netem delay "${latency1}" $deviation1 loss "${packet_loss1}" || echo error at line $LINENO
 
-		tc class add dev $DEV parent 1:1 classid 1:12 hfsc ls rate "${down2}" ul rate "${down2}" || echo error at line $LINENO
-		tc qdisc add dev $DEV parent 1:12 handle 30 netem delay "${latency2}" $deviation2 loss "${packet_loss2}" || echo error at line $LINENO
+			tc class add dev $DEV parent 1:1 classid 1:12 hfsc ls rate "${down2}" ul rate "${down2}" || echo error at line $LINENO
+			tc qdisc add dev $DEV parent 1:12 handle 30 netem delay "${latency2}" $deviation2 loss "${packet_loss2}" || echo error at line $LINENO
 
-		# All classes get stochastic fairness
-		tc qdisc add dev $DEV parent 1:10 handle 100: sfq perturb 10 || echo error at line ${LINENO}
-		tc qdisc add dev $DEV parent 20: handle 101: sfq perturb 10 || echo error at line ${LINENO}
-		tc qdisc add dev $DEV parent 30: handle 102: sfq perturb 10 || echo error at line ${LINENO}
+			# All classes get stochastic fairness
+			tc qdisc add dev $DEV parent 1:10 handle 100: sfq perturb 10 || echo error at line ${LINENO}
+			tc qdisc add dev $DEV parent 20: handle 101: sfq perturb 10 || echo error at line ${LINENO}
+			tc qdisc add dev $DEV parent 30: handle 102: sfq perturb 10 || echo error at line ${LINENO}
 
-		# Ingress queueing to limit upload from LAN
-		ip link set dev ifb0 down
-		modprobe ifb numifbs=1
-		ip link set dev ifb0 up
+			# Ingress queueing to limit upload from LAN
+			ip link set dev ifb0 down
+			modprobe ifb numifbs=1
+			ip link set dev ifb0 up
 
-		# Start clean
-		tc qdisc del dev $DEV ingress > /dev/null 2>&1
-		tc qdisc del dev ifb0 root > /dev/null 2>&1
+			# Start clean
+			tc qdisc del dev $DEV ingress > /dev/null 2>&1
+			tc qdisc del dev ifb0 root > /dev/null 2>&1
 
-		tc qdisc add dev $DEV handle ffff: ingress
+			tc qdisc add dev $DEV handle ffff: ingress
 
-		# Limit incoming traffic on internal LAN interface: limit upload
-		tc qdisc add dev ifb0 parent root handle 2: hfsc default 10 || echo error at line $LINENO
-		tc class add dev ifb0 parent 2: classid 2:1 hfsc ls rate 100mbit ul rate 100mbit || echo error at line $LINENO
-		tc class add dev ifb0 parent 2:1 classid 2:10 hfsc ls rate 90mbit ul rate 90mbit || echo error at line $LINENO
-		tc class add dev ifb0 parent 2:1 classid 2:11 hfsc ls rate "${up1}" ul rate "${up1}" || echo error at line $LINENO
-		tc class add dev ifb0 parent 2:1 classid 2:12 hfsc ls rate "${up2}" ul rate "${up2}" || echo error at line $LINENO
+			# Limit incoming traffic on internal LAN interface: limit upload
+			tc qdisc add dev ifb0 parent root handle 2: hfsc default 10 || echo error at line $LINENO
+			tc class add dev ifb0 parent 2: classid 2:1 hfsc ls rate 100mbit ul rate 100mbit || echo error at line $LINENO
+			tc class add dev ifb0 parent 2:1 classid 2:10 hfsc ls rate 90mbit ul rate 90mbit || echo error at line $LINENO
+			tc class add dev ifb0 parent 2:1 classid 2:11 hfsc ls rate "${up1}" ul rate "${up1}" || echo error at line $LINENO
+			tc class add dev ifb0 parent 2:1 classid 2:12 hfsc ls rate "${up2}" ul rate "${up2}" || echo error at line $LINENO
 
-		tc qdisc add dev ifb0 parent 2:10 handle 200: sfq perturb 10 || echo error at line $LINENO
-		tc qdisc add dev ifb0 parent 2:11 handle 201: sfq perturb 10 || echo error at line $LINENO
-		tc qdisc add dev ifb0 parent 2:12 handle 202: sfq perturb 10 || echo error at line $LINENO
+			tc qdisc add dev ifb0 parent 2:10 handle 200: sfq perturb 10 || echo error at line $LINENO
+			tc qdisc add dev ifb0 parent 2:11 handle 201: sfq perturb 10 || echo error at line $LINENO
+			tc qdisc add dev ifb0 parent 2:12 handle 202: sfq perturb 10 || echo error at line $LINENO
 
-		# Satellite 1
-		# Limit Download
-		tc filter add dev $DEV parent 1: protocol ip u32 match ip dst "${WANIP1}" flowid 1:11 || echo error at line $LINENO
-		# Limit Upload
-		tc filter add dev $DEV parent ffff: protocol ip u32 match ip src "${WANIP1}" action mirred egress redirect dev ifb0
-		tc filter add dev ifb0 parent 2: protocol ip u32 match ip src "${WANIP1}" flowid 2:11 || echo error at line $LINENO
+			# Satellite 1
+			# Limit Download
+			tc filter add dev $DEV parent 1: protocol ip u32 match ip dst "${WANIP1}" flowid 1:11 || echo error at line $LINENO
+			# Limit Upload
+			tc filter add dev $DEV parent ffff: protocol ip u32 match ip src "${WANIP1}" action mirred egress redirect dev ifb0
+			tc filter add dev ifb0 parent 2: protocol ip u32 match ip src "${WANIP1}" flowid 2:11 || echo error at line $LINENO
 
-		# Satellite 2
-		# Limit Download
-		tc filter add dev $DEV parent 1: protocol ip u32 match ip dst "${WANIP2}" flowid 1:12 || echo error at line $LINENO
-		# Limit Upload
-		tc filter add dev $DEV parent ffff: protocol ip u32 match ip src "${WANIP2}" action mirred egress redirect dev ifb0
-		tc filter add dev ifb0 parent 2: protocol ip u32 match ip src "${WANIP2}" flowid 2:12 || echo error at line $LINENO
-	done
-fi
+			# Satellite 2
+			# Limit Download
+			tc filter add dev $DEV parent 1: protocol ip u32 match ip dst "${WANIP2}" flowid 1:12 || echo error at line $LINENO
+			# Limit Upload
+			tc filter add dev $DEV parent ffff: protocol ip u32 match ip src "${WANIP2}" action mirred egress redirect dev ifb0
+			tc filter add dev ifb0 parent 2: protocol ip u32 match ip src "${WANIP2}" flowid 2:12 || echo error at line $LINENO
+		done
+		;;
+esac
 
 echo "$timestamp: $base: traffic shaper completed" >> $log
